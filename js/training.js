44443eb51da4ca8c014b4707231c22c34dd94cf4
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-08-31 13:34:46 +0200

    exe /5 finiched

--- js/training.js
+++ js/training.js
@@ -10,8 +10,8 @@ document.querySelector("#ex1 .button").addEventListener("click", () => {
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
-const timer = setInterval (() => {
-    document.querySelector("#ex2-scroll-value").innerHTML = (scrollY)
+setInterval (() => {
+    document.querySelector("#ex2-scroll-value").innerHTML = scrollY
 },100)
 
 /* ------------------------------------ */
@@ -26,22 +26,22 @@ document.querySelector("#ex3 .button").addEventListener("click", function(){
 /* --- Exercice 4 --- */
 
 
-let count = 0
+let count = 0;
 // Function for add class in css for change background-color on 3 times.
 function toggleChangeColor(event) {
     if (count === 0) {
-    this.classList.remove("button-green")
+    this.classList.remove("button-green");
     this.classList.toggle("button-blue");
     count++;
 }
 else if (count === 1) {
     this.classList.remove("button-blue");
-    this.classList.toggle("button-red")
+    this.classList.toggle("button-red");
     count++;
 }
 else {
-    this.classList.remove("button-red")
-    this.classList.toggle("button-green")
+    this.classList.remove("button-red");
+    this.classList.toggle("button-green");
     count = 0;
 }
 }
@@ -51,7 +51,22 @@ else {
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
+// Function for get random color 
+function getRandomRgb (event) {
+    let randomColor = [];
+    for (let i = 0; i < 3; i++){
+        randomColor[i] = Math.floor(Math.random() * (255 - 0)) ;
+    }
+    return `rgb(${randomColor.join(',')})`
+}
+console.log(getRandomRgb())
 
+
+for (let i of document.querySelectorAll(".flex-list .button")) {
+    console.log(i)
+    i.addEventListener('mouseenter', function() {
+        i.setAttribute("style",`background-color: ${getRandomRgb()};`);
+})}
 
 /* ------------------------------------ */
 /* --- Exercice 6 --- */
